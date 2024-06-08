c63f2846dd1a4357e35a4607223d483c4613f500
ZhenRB <rolando.sotomayor.b@uni.pe>
2024-06-07 21:09:22 -0500

    Modified: query

--- Querys SQLSM/InsertarCategoria.sql
+++ Querys SQLSM/InsertarCategoria.sql
@@ -1,23 +1,24 @@
+-- Categoria
 INSERT INTO dbo.categoria (id_category, category_count, category_description, category_type)
-VALUES (1, 114, 'Libros', 'LIB')
+VALUES (1, 114, 'Libros', 'LIB');
 
 INSERT INTO dbo.categoria (id_category, category_count, category_description, category_type)
-VALUES (2, 9, 'Accesorios', 'ACC')
+VALUES (2, 9, 'Accesorios', 'ACC');
 
 INSERT INTO dbo.categoria (id_category, category_count, category_description, category_type)
-VALUES (3, 8, 'Papelería', 'PAP')
+VALUES (3, 8, 'Papelería', 'PAP');
 
 INSERT INTO dbo.categoria (id_category, category_count, category_description, category_type)
-VALUES (4, 2, 'Accesorios de computación', 'COM')
+VALUES (4, 2, 'Accesorios de computación', 'COM');
 
 INSERT INTO dbo.categoria (id_category, category_count, category_description, category_type)
-VALUES (5, 24, 'Tecnología', 'TEC')
+VALUES (5, 24, 'Tecnología', 'TEC');
 
 INSERT INTO dbo.categoria (id_category, category_count, category_description, category_type)
-VALUES (6, 4, 'Bolsas y Mochilas', 'BYM')
+VALUES (6, 4, 'Bolsas y Mochilas', 'BYM');
 
 INSERT INTO dbo.categoria (id_category, category_count, category_description, category_type)
-VALUES (7, 9, 'Ropa', 'ROP')
+VALUES (7, 9, 'Ropa', 'ROP');
 
 
 
