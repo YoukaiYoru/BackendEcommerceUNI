c7d5ca9afcdc08c26e0ed0bf041d299470401ebd
YoukaiYoru <edward.oliva.l@uni.pe>
2024-06-14 19:37:05 -0500

    Restore: Frontend folder

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,10 @@
 <template>
   <v-app>
-
-    <v-nav-bar >
-      <v-nav-item to="/">Home</v-nav-item>
-      <v-nav-item to="/about">About</v-nav-item>
-      <v-nav-item to="/contact">Contact</v-nav-item>
-    </v-nav-bar>
     <v-main>
       <router-view />
     </v-main>
   </v-app>
 </template>
-
-
-<style scoped>
-
-v-nav-bar {
-
-  background-color: aqua;
-}
-
-</style>
 
 <script setup>
   //
